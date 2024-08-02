11a7b9c8b49c94373c7c74c2f11acc170765f2ca
devsai9 <saisiddhish@gmail.com>
2024-08-01 23:02:28 -0400

    Addressed typo in teams section & added images

--- src/app/components/team/team.js
+++ src/app/components/team/team.js
@@ -4,24 +4,24 @@ import Link from 'next/link';
 
 export default function TeamSection() {
     let teamCards = [
-        ["Aryan Sharma", "Director", "/headshots/2025aryan_headshot.jpg"],
-        ["Victor Liu", "Director", "/headshots/2025victor_headshot.jpg"],
+        ["Aryan Sharma", "Director", "/headshots/aryan_headshot.jpg"],
+        ["Victor Liu", "Director", "/headshots/victor_headshot.jpg"],
         
-        ["Joshua Oh", "Graphic Design Lead", "/headshots/2025joshua_headshot.jpeg"],
-        ["Aditri Maiti", "Graphic Design", "/headshots/2025aditri_headshot.jpg"],
-        ["Anjali Vallabheneni", "Graphic Design", "/headshots/2025anjali_headshot.jpg"],
-        ["Jiwon Kim", "Graphic Design", "/headshots/2025jiwon_headshot.png"],
+        ["Joshua Oh", "Graphic Design Lead", "/headshots/joshua_headshot.jpeg"],
+        ["Aditri Maiti", "Graphic Design", "/headshots/aditri_headshot.jpg"],
+        ["Anjali Vallabhaneni", "Graphic Design", "/headshots/anjali_headshot.jpg"],
+        ["Jiwon Kim", "Graphic Design", "/headshots/jiwon_headshot.png"],
         
-        ["Kevin Yue", "Sponsor", "/headshots/2025kevin_headshot.png"],
-        ["Rana Cherukuri", "Sponsor", "/headshots/2025rana_headshot.png"],
-        ["Vinay Vimalkumar", "Sponsor", "/headshots/2025vinay_headshot.png"],
+        ["Kevin Yue", "Sponsor", "/headshots/kevin_headshot.png"],
+        ["Rana Cherukuri", "Sponsor", "/headshots/rana_headshot.png"],
+        ["Vinay Vimalkumar", "Sponsor", "/headshots/vinay_headshot.png"],
 
-        ["Sai Chandra", "Website Lead", "/headshots/2025sai_headshot.jpg"],
-        ["Mark Shi", "Website", "/headshots/2025mark_headshot.jpg"],
+        ["Sai Chandra", "Website Lead", "/headshots/sai_headshot.jpg"],
+        ["Mark Shi", "Website", "/headshots/mark_headshot.jpg"],
 
-        ["Jaden Li", "Workshop Lead", "/headshots/2025jaden_headshot.jpg"],
-        ["Suhas Anumolu", "Workshop", "/headshots/2025suhas_headshot.jpg"],
-        ["Zak Mazerski", "Workshop", "/headshots/2025zak_headshot.jpg"],
+        ["Jaden Li", "Workshop Lead", "/headshots/jaden_headshot.jpg"],
+        ["Suhas Anumolu", "Workshop", "/headshots/suhas_headshot.jpg"],
+        ["Zak Mazerski", "Workshop", "/headshots/zak_headshot.jpg"],
     ]
 
     teamCards = teamCards.map((card) => {
