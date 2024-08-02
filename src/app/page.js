e483c173bf0a465f3d8b007fc16695963a9c66a4
devsai9 <saisiddhish@gmail.com>
2024-08-01 23:17:10 -0400

    Fixed more image paths

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
         <p>📢  Want to help make QuHacks possible? Donate <a href="https://hcb.hackclub.com/donations/start/quhacks-2024" rel="noreferrer noopener" target="_blank">HERE</a>!</p>
       </div>
       <div className={styles.homecontainer}>
-          <Image alt='QuHacks Logo' src="/logo.png" width={100} height={100} sizes='(max-width: 750px) 15rem, 20rem' className={styles.logo} />
+          <Image alt='QuHacks Logo' src="logo.png" width={100} height={100} sizes='(max-width: 750px) 15rem, 20rem' className={styles.logo} />
           <div>
             <span className={styles.title}>QuHacks 2025</span>
             <p>Coming Soon</p>
