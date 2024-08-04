5253662dcedc11e130946cb204a03445c3fb3f23
Aryan Sharma <74842047+aryans-15@users.noreply.github.com>
2024-08-04 19:38:37 -0400

    Update team.js

--- src/app/components/team/team.js
+++ src/app/components/team/team.js
@@ -11,7 +11,8 @@ export default function TeamSection() {
         ["Aditri Maiti", "Graphic Design", "headshots/aditri_headshot.jpg"],
         ["Anjali Vallabhaneni", "Graphic Design", "headshots/anjali_headshot.jpg"],
         ["Jiwon Kim", "Graphic Design", "headshots/jiwon_headshot.png"],
-        
+
+        ["Barry Chen", "Sponsor", "headshots/barry_headshot.jpg"],
         ["Kevin Yue", "Sponsor", "headshots/kevin_headshot.png"],
         ["Rana Cherukuri", "Sponsor", "headshots/rana_headshot.png"],
 
