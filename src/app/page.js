48ab0bf723311ce1cc7b7e5e0035d4422dc6e588
aryans0714 <aryan.sharma0714@gmail.com>
2024-08-20 17:42:56 -0400

    updated sponsors

--- src/app/page.js
+++ src/app/page.js
@@ -58,7 +58,7 @@ export default function Home() {
           <Image alt='QuHacks Logo' src="logo.png" width={100} height={100} sizes='(max-width: 750px) 15rem, 20rem' className={styles.logo} />
           <div>
             <span className={styles.title}>QuHacks 2025</span>
-            <p>December 14th @ Johns Hopkins APL (Building 201)</p>
+            <p>December 14th, 2024 @ Johns Hopkins APL (Building 201)</p>
             <div className={styles.btngroup}>
               {/* Logic for register: onClick={() => setShowRegisterModal(true)} */}
               <button className='btn-primary' disabled={true}>Register!</button>
